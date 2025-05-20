5750b16774dae2756fff5d04a273397c4b0a210b
soyvillareal <sebastiangarces152@gmail.com>
2025-05-20 17:16:43 -0500

    fix: bump version to 1.0.2 in package.json and package-lock.json; update exports in index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,8 @@ export { WebGL2 } from './lib/WebGL2';
 
 export { type Transformer } from './lib/Utils/Transformer';
 export { type Shape2D } from './lib/Common/Shape2D';
+
+export type * from "./lib/Types/Arguments"
+export type * from "./lib/Types/Global";
+export type * from "./lib/Types/Shapes";
+export type * from "./lib/Types/Worker";
